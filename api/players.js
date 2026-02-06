9da29cf41f20c987b9e97b8b73064ff82d198868
aminemech <48395151+aminemech@users.noreply.github.com>
2026-02-06 21:07:06 +0100

    Update players.js

--- api/players.js
+++ api/players.js
@@ -1,16 +1,11 @@
+// api/players.js
 export default async function handler(req, res) {
   try {
-    const CFX_ID = "loydpv";
-
-    const response = await fetch(
-      `https://servers-frontend.fivem.net/api/servers/single/${CFX_ID}`
-    );
-
-    const data = await response.json();
-    const players = data.Data?.players || [];
+    const response = await fetch("http://194.50.0.52:30120/players.json");
+    const players = await response.json(); // real-time players
 
     res.status(200).json(players);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch players" });
+  } catch (err) {
+    res.status(500).json({ error: "Failed to load players" });
   }
 }
